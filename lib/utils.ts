dc4b4765cccac1568bea5098a0e9f533c534884b
tom1130chen-tech <tom1130chen@gmail.com>
2026-03-03 17:55:05 -0500

    Add utility functions cn and uid

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,1 +1,9 @@
+import clsx, { type ClassValue } from "clsx";
 
+export function cn(...inputs: ClassValue[]) {
+  return clsx(inputs);
+}
+
+export function uid(prefix = "card") {
+  return `${prefix}-${Math.random().toString(16).slice(2)}-${Date.now().toString(16)}`;
+}
