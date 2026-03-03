aaacdd1872f59d18bba556f319f0baf933ad9b66
tom1130chen-tech <tom1130chen@gmail.com>
2026-03-03 17:55:51 -0500

    Update Input.tsx

--- components/ui/Input.tsx
+++ components/ui/Input.tsx
@@ -1,1 +1,18 @@
+"use client";
 
+import type { InputHTMLAttributes } from "react";
+import { cn } from "@/lib/utils";
+
+export function Input({ className, ...props }: InputHTMLAttributes<HTMLInputElement>) {
+  return (
+    <input
+      className={cn(
+        "w-full bg-white text-pencil text-lg font-body px-3 py-2 border-[3px] border-pencil",
+        "[border-radius:var(--r-wobbly)]",
+        "focus:outline-none focus:border-penblue focus:ring-2 focus:ring-penblue/20",
+        className
+      )}
+      {...props}
+    />
+  );
+}
